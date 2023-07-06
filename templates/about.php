9f5aca1ae3664f82a0c547d95fb89b6eb176c71c
JesusFranco04 <francojesus023@gmail.com>
2023-07-06 14:53:02 -0500

    hola

--- templates/about.php
+++ templates/about.php
@@ -17,21 +17,38 @@
               <div class="row justify-content-center">
                 <div class="col-md-6 bg-secondary bg-gradient text-center">
                   <form>
-                    <div class="form-group mt-3">
-                      <label for="nombre" class="text-white">Nombre</label>
-                      <input type="text" class="form-control" id="nombre" placeholder="Ingresa tu nombre">
-                    </div>
-                    <div class="form-group">
-                      <label for="correo" class="text-white">Correo Electrónico</label>
-                      <input type="email" class="form-control" id="correo" placeholder="Ingresa tu correo electrónico">
+                  <div class="form-group mt-3">
+                      <label for="cedula" class="text-white">Cedula</label>
+                      <input type="tel" class="form-control" id="cedula" placeholder="Ingresa su cedula">
                     </div>
                     <div class="form-group mt-3">
-                      <label for="telefono" class="text-white">Telefono</label>
-                      <input type="tel" class="form-control" id="telefono" placeholder="Ingresa su numero de telefono">
+                      <label for="nombre" class="text-white">Nombres y Apellidos</label>
+                      <input type="text" class="form-control" id="nombre" placeholder="Ingresa tu nombres y apellidos">
                     </div>
+                    
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="form-group mt-3">
+                          <label for="telefono" class="text-white">Teléfono</label>
+                          <input type="tel" class="form-control" id="telefono" placeholder="Ingresa su número de teléfono">
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <div class="form-group mt-3">
+                          <label for="operadora" class="text-white">Operadora</label>
+                          <select class="form-control" id="operadora">
+                          <option value="">Seleccionar Operadora</option>
+                            <option value="claro">Claro</option>
+                            <option value="movistar">Movistar</option>
+                            <!-- Agrega más opciones de operadoras aquí si es necesario -->
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                      
                     <div class="form-group">
-                      <label for="mensaje" class="text-white">Mensaje</label>
-                      <textarea class="form-control" id="mensaje" rows="5" placeholder="Escribe tu mensaje"></textarea>
+                      <label for="mensaje" class="text-white">Descripcion</label>
+                      <textarea class="form-control" id="mensaje" rows="5" placeholder="Escribe tu mensaje de su consulta"></textarea>
                     </div>
                     <button type="submit" class="btn btn-primary mt-2 w-50">Enviar</button>
                   </form><br>
